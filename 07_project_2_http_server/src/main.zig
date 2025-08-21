d7502eaf822de97cf5a198c61eb88948b35b6acd
shybyte <shybyte@users.noreply.github.com>
2025-08-21 12:07:02 +0200

    Add HTTP server implementation for Chapter 7

---
+++ 07_project_2_http_server/src/main.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const SocketConf = @import("config.zig");
+const Request = @import("request.zig");
+const Response = @import("response.zig");
+const Method = Request.Method;
+const stdout = std.io.getStdOut().writer();
+
+pub fn main() !void {
+    const socket = try SocketConf.Socket.init();
+    try stdout.print("Server Addr: {any}\n", .{socket._address});
+    var server = try socket._address.listen(.{});
+    const connection = try server.accept();
+    var buffer: [1000]u8 = .{0} ** 1000;
+    _ = try Request.read_request(connection, buffer[0..buffer.len]);
+    try stdout.print("{s}\n", .{buffer});
+
+    const request = Request.parse_request(buffer[0..buffer.len]);
+    try stdout.print("{any}\n", .{request});
+
+    if (request.method == Method.GET) {
+        if (std.mem.eql(u8, request.uri, "/")) {
+            try Response.send_200(connection);
+        } else {
+            try Response.send_404(connection);
+        }
+    }
+}
